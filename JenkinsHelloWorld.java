46e895aa85dad9fcca3b8f5efbe81d511b5af0e4
johnjana <52849382+johnjana@users.noreply.github.com>
2023-03-11 20:35:55 +0900

    update

--- JenkinsHelloWorld.java
+++ JenkinsHelloWorld.java
@@ -1,6 +1,6 @@
 class JenkinsHelloWorld {
   
-  public static void main(){
+   public static void main(String[] args){
     System.out.println("This is a sample class to test the running of java application");
   }
   
