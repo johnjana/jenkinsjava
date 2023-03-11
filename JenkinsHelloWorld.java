12e4ef0648d7e38de0531c480c321c7d9b78eeff
johnjana <52849382+johnjana@users.noreply.github.com>
2023-03-11 20:34:59 +0900

    3 commit

--- JenkinsHelloWorld.java
+++ JenkinsHelloWorld.java
@@ -1,3 +1,7 @@
 class JenkinsHelloWorld {
-  System.out.println("This is a sample class to test the running of java application");
+  
+  public static void main(){
+    System.out.println("This is a sample class to test the running of java application");
+  }
+  
 }
